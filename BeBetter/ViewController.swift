78e30f09dcf0e60f9e648182e8f312a539d88760
anacarolsfcabral <aninha_carolcabral@hotmail.com>
2015-07-21 11:26:19 -0300

    First Commit

---
+++ BeBetter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeBetter
+//
+//  Created by Ana Carolina Cabral on 21/07/15.
+//  Copyright (c) 2015 Ana Carolina Cabral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
